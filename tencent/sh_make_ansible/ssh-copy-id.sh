2d90f08c3a960f0871931e9febc5896bfa8789c8
Monpati <59530377+Monpati@users.noreply.github.com>
2024-10-16 16:44:17 +0800

    Update ssh-copy-id.sh

--- tencent/sh_make_ansible/ssh-copy-id.sh
+++ tencent/sh_make_ansible/ssh-copy-id.sh
@@ -2,5 +2,5 @@
 while IFS= read -r line
 do
   ip=$(echo "$line" | cut -d ':' -f 2)
-  sshpass -p 'Kc^cpUh09c$f!G9F' ssh-copy-id -o StrickHostKeyChecking "$ip"
+  sshpass -p '' ssh-copy-id -o StrickHostKeyChecking "$ip"
 done < /root/ansible_node config
